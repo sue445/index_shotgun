08d670eeb0a2592b92f1ff9a767e4d2f73fa3851
sue445 <sue445@sue445.net>
2015-09-07 02:32:02 +0900

    Resolve Style/Next: Use next to skip iteration

--- lib/index_shotgun/analyzer.rb
+++ lib/index_shotgun/analyzer.rb
@@ -9,19 +9,19 @@ module IndexShotgun
         indexes = table_indexes(table)
 
         indexes.permutation(2).each_with_object([]) do |(source_index, target_index), response|
-          if source_index.columns.start_with?(target_index.columns)
-            if target_index.unique
-              last_column = source_index.columns.last
-              response << {
-                index:  source_index,
-                result: "#{source_index.name} has unnecessary column #{last_column} (#{target_index.name} is unique index!)",
-              }
-            else
-              response << {
-                index:  target_index,
-                result: "#{target_index.name} is a left-prefix of #{source_index.name}",
-              }
-            end
+          next unless source_index.columns.start_with?(target_index.columns)
+
+          if target_index.unique
+            last_column = source_index.columns.last
+            response << {
+              index:  source_index,
+              result: "#{source_index.name} has unnecessary column #{last_column} (#{target_index.name} is unique index!)",
+            }
+          else
+            response << {
+              index:  target_index,
+              result: "#{target_index.name} is a left-prefix of #{source_index.name}",
+            }
           end
         end
       end
