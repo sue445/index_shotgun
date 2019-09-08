ba1fd24a0e43eea6f6ab46af9e576e322e869994
sue445 <sue445@sue445.net>
2019-09-08 15:39:12 +0900

    Migrate to GitHub Actions (rubocop)

--- ci/install.sh
+++ ci/install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -xe
 
-if [ ${DATABASE} = "mysql" ]; then
+if [ "${DATABASE}" = "mysql" ]; then
   bundle install --path vendor/bundle/ --jobs 4 --retry 3 --without oracle postgresql sqlite3
 
-elif [ ${DATABASE} = "postgresql" ]; then
+elif [ "${DATABASE}" = "postgresql" ]; then
   bundle install --path vendor/bundle/ --jobs 4 --retry 3 --without mysql oracle sqlite3
 
-elif [ ${DATABASE} = "sqlite3" ]; then
+elif [ "${DATABASE}" = "sqlite3" ]; then
   bundle install --path vendor/bundle/ --jobs 4 --retry 3 --without mysql oracle postgresql
 
-elif [ ${DATABASE} = "oracle" ]; then
+elif [ "${DATABASE}" = "oracle" ]; then
   bundle install --path vendor/bundle/ --jobs 4 --retry 3 --without mysql postgresql sqlite3
+
+else
+  bundle install --path vendor/bundle/ --jobs 4 --retry 3 --without mysql postgresql sqlite3 oracle
 
 fi
